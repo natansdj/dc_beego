d9d0263befb61ace50a4a1112476c4811210df5e
natan <natansidjabat@gmail.com>
2019-02-07 19:14:36 +0700

    cleanup

--- etc/fake-web/auth0-variables.js
+++
@@ -1,4 +0,0 @@
-var AUTH0_CLIENT_ID='bA0DhS5DPKbW3ZiaZvhygofRL8a_96tz';
-var AUTH0_DOMAIN='nth-local.auth0.com';
-var AUTH0_AUDIENCE = 'https://nth-local.auth0.com/api/v2/';
-var AUTH0_CALLBACK_URL = location.href;
