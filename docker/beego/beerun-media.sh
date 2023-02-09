f66cc9f274fcd731b7ffe5ecc6781421e3742acf
natan <natansdj@users.noreply.github.com>
2023-02-10 03:47:07 +0700

    update

---
+++ docker/beego/beerun-media.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+WORKDIR="/go/src/${GO_SVC}"
+
+#
+if [ -z "$WORKDIR" ]; then
+    echo "Empty WORKDIR!!!"
+    exit 1
+fi
+
+#
+cd ${WORKDIR}
+echo "WORKDIR : ${WORKDIR}"
+
+go mod download
+go mod vendor
+
+#
+if [ -z "$GO_SVC" ]
+then
+  echo "Empty GO_SVC!!!"
+  exit 1
+else
+  echo "GO_SVC : ${GO_SVC}"
+  GOOS=linux GOARCH=amd64 go build && ./${GO_SVC}
+fi
